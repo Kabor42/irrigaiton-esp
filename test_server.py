1e80135d91540c8b341cda20e910498d5d78c90f
Nagy Dominik <n.dominik@zoho.com>
2022-06-14 22:41:08 +0200

    Add web page dev

---
+++ test_server.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+
+import os
+from time import time, sleep
+
+from flask import Flask, request, abort, render_template
+from flask.json import jsonify, loads
+
+app = Flask(__name__)
+
+zones = {
+    "1": False,
+    "2": False,
+    "3": False,
+    "4": False,
+    "5": False,
+    "6": False,
+    "7": False,
+    "8": False,
+}
+
+h_time = 0
+local_time = time()
+offset_gmt = 0
+config = None
+sched = None
+buttons = None
+
+host_name = "ESPrinkler2"
+
+sched = """{"scedules":[{ "zone": "Zone 2", "startTime": "23:52", "duration": 15, "days": [ "Monday", "Tuesday", "Thursday", "Friday" ], "begin": "2022-01-12", "end": "2022-01-28" }]}"""
+sched = loads(sched.strip('\n'))
+
+
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+
+@app.route("/status")
+def status():
+    global h_time
+    global local_time
+    h_time += time() - local_time
+    local_time = time()
+    payload = {"time": int(h_time), "host": host_name, "offsetGMT": offset_gmt}
+    for key, value in zones.items():
+        payload[f"zone{key}"] = value
+    print(payload)
+    return payload
+
+
+@app.route("/settime")
+def set_time():
+    r_time = int(request.args.get("time"))
+    r_offset_gmt = int(request.args.get("offset"))
+    print(f"Time:[{r_time}], Offset GMT:[{r_offset_gmt}]")
+
+    if r_time > 0 and r_offset_gmt > 0:
+        global local_time
+        local_time = r_time
+        global offset_gmt
+        offset_gmt = r_offset_gmt
+    else:
+        return jsonify({"error": True, "message": "Wrong format!"})
+    return jsonify(success=True)
+
+
+@app.route("/clear")
+def clear_zones():
+    global zones
+    for _, zone in zones.items():
+        zone = False
+    return jsonify(status=True)
+
+
+@app.route("/clean")
+def clean_all():
+    global config
+    global sched
+    global buttons
+    config = None
+    sched = None
+    buttons = None
+    return jsonify({"error": False, "message": "Persistent storage has been cleaned!"})
+
+
+@app.route("/toggle/<string:zone>")
+def toggle(zone: str):
+    global zones
+    if zone not in zones.keys():
+        print(f"No such zone {zone}")
+        return jsonify({"error": True, "message": "No such zone"})
+    zones[zone] = not zones[zone]
+    print(zones)
+    return jsonify(status=True)
+
+
+@app.route("/restart")
+def restart():
+    print("Restarting ...")
+    sleep(10)
+    return jsonify(status=True)
+
+
+@app.route("/list")
+def list():
+    return jsonify(
+        [
+            {"type": "file" if os.path.isfile(file) else "dir", "name": file}
+            for file in os.listdir(".")
+        ]
+    )
+
+
+@app.route("/config.json")
+def get_config_json():
+    global config
+    if config is None:
+        abort(404)
+    return jsonify(config)
+
+
+@app.route("/sched.json")
+def get_sched_json():
+    global sched
+    if sched is None:
+        abort(404)
+    return jsonify(sched)
+
+
+@app.route("/buttons.json")
+def get_buttons_json():
+    global buttons
+    if buttons is None:
+        abort(404)
+    return jsonify(buttons)
+
+
+@app.route("/edit", methods=["DELETE"])
+def delete_file():
+    pass
+
+
+@app.route("/edit", methods=["PUT"])
+def create_file():
+    pass
+
+
+@app.route("/edit", methods=["POST"])
+def modify_file():
+    pass
+
+
+@app.route("/update", methods=["POST"])
+def update_file():
+    pass
+
+
+if __name__ == "__main__":
+    h_time = 0
+    app.run(host="0.0.0.0", port="8088", debug=True)
---
+++ testserver.py
@@ -0,0 +1,278 @@
+#
+# ESPrinkler2 testserver.py
+#
+# This is a smiple server so data-uncompressed can be tested
+# and edited using a local browser to http://localhost
+# Note that data-uncompressed does not save config.json nor sched.json
+# This server saves that data as strings
+# Similarly current time and zone status are simulated
+#
+import SimpleHTTPServer
+from urlparse import urlparse, parse_qs
+import SocketServer
+import os
+import time
+import json
+import cgi
+
+zones = [
+        'off', 'off', 'off', 'off',
+        'off', 'off', 'off', 'off'
+        ]
+
+htime = 0
+ltime = time.time()
+offsetGMT = 0
+config = None
+sched = None
+buttons = None
+host = 'ESPrinkler2'
+
+
+class ESPrinkler2RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        def xsend(self, content, contentType="text/html"):
+            print "serving custom response: " + content
+            self.send_response(200)
+            self.send_header("Content-Type", contentType)
+            v = len(content)
+            self.send_header("Content-Tength", v)
+            self.end_headers()
+            self.wfile.write(content)
+
+        def do_GET(self):
+            global zones, htime, ltime, config, sched, host, buttons, offsetGMT
+            print self.path
+            query_components = parse_qs(urlparse(self.path).query)
+            self.path = self.path.split('?', 1)[0]
+            print self.path
+            if self.path == '/status':
+                htime += time.time() - ltime
+                ltime = time.time()
+                v = {'time': int(htime), 'host': host, 'offsetGMT': offsetGMT}
+                for i in range(0, 8):
+                    v['zone'+str(i)] = zones[i]
+                self.xsend(json.dumps(v), "text/json")
+            elif self.path == '/settime':
+                htime = 0
+                try:
+                    htime = int(query_components['time'][0])
+                except:
+                    pass
+                ltime = time.time()
+                try:
+                    o = int(query_components['offset'][0])
+                    offsetGMT = o
+                except:
+                    pass
+                self.xsend("ok")
+            elif self.path == '/clear':
+                for i in range(0, 8):
+                    zones[i] = 'off'
+                self.xsend("ok")
+            elif self.path == '/clean':
+                config = None
+                sched = None
+                buttons = None
+                self.xsend("Persistant Storage has been cleaned.")
+            elif self.path == '/toggle':
+                i = 0
+                try:
+                    i = int(query_components['zone'][0])
+                except:
+                    pass
+                print i
+                z = zones[i]
+                if z == 'on':
+                    z = 'off'
+                else:
+                    z = 'on'
+                zones[i] = z
+                self.xsend("ok")
+            elif self.path == '/restart':
+                self.xsend(
+                    "Restarting... please wait a minute or two and refresh")
+            elif self.path == '/list':
+                v = []
+                for f in os.listdir('.'):
+                    v.append({'type': 'file', 'name': f})
+                self.xsend(json.dumps(v), "text/json")
+            # config.json is not saved, but only temp string storage
+            elif self.path == '/config.json':
+                if config is None:
+                    self.send_error(404, "File not found")
+                    return None
+                else:
+                    print('config sending\n'+config)
+                    self.xsend(config)
+            # sched.json is not saved, but only temp string storage
+            elif self.path == '/sched.json':
+                if sched is None:
+                    self.send_error(404, "File not found")
+                    return None
+                else:
+                    print('sched sending\n'+sched)
+                    self.xsend(sched)
+            # buttons.json is not saved, but only temp string storage
+            elif self.path == '/buttons.json':
+                if buttons is None:
+                    self.send_error(404, "File not found")
+                    return None
+                else:
+                    print('buttons sending\n'+buttons)
+                    self.xsend(buttons)
+            else:
+                return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+        def do_DELETE(self):
+            self.path = self.path.split('?', 1)[0]
+            print self.path
+            if self.path == '/edit':
+                form = cgi.FieldStorage(
+                    fp=self.rfile,
+                    headers=self.headers,
+                    environ={'REQUEST_METHOD': 'POST',
+                             'CONTENT_TYPE': self.headers['Content-Type'],
+                             })
+
+                for field in form.keys():
+                    field_item = form[field]
+                    if field == 'path':
+                        fn = field_item.value
+                        if fn.startswith('/'):
+                            fn = fn[1:]
+                        if os.path.isfile(fn):
+                            os.remove(fn)
+                            self.xsend("")
+                            return
+
+            self.send_response(404)
+
+        def do_PUT(self):
+            self.path = self.path.split('?', 1)[0]
+            print self.path
+            if self.path == '/edit':
+                form = cgi.FieldStorage(
+                    fp=self.rfile,
+                    headers=self.headers,
+                    environ={'REQUEST_METHOD': 'POST',
+                             'CONTENT_TYPE': self.headers['Content-Type'],
+                             })
+
+                for field in form.keys():
+                    field_item = form[field]
+                    if field == 'path':
+                        fn = field_item.value
+                        if fn.startswith('/'):
+                            fn = fn[1:]
+                        if not os.path.isfile(fn):
+                            f = open(fn, 'w')
+                            f.close()
+                            self.xsend("")
+                            return
+                        else:
+                            self.send_response(404)
+                            self.end_headers()
+                            return
+
+            self.send_response(404)
+
+        def do_POST(self):
+            global config, sched, buttons, host, offsetGMT
+            self.path = self.path.split('?', 1)[0]
+            print self.path
+            if self.path == '/edit':
+
+                form = cgi.FieldStorage(
+                    fp=self.rfile,
+                    headers=self.headers,
+                    environ={'REQUEST_METHOD': 'POST',
+                             'CONTENT_TYPE': self.headers['Content-Type'],
+                             })
+
+                for field in form.keys():
+                    field_item = form[field]
+                    if field_item.filename:
+                        # The field contains an uploaded file
+                        file_data = field_item.file.read()
+                        file_len = len(file_data)
+                        print('Uploaded %s as "%s" (%d bytes)' %
+                              (field, field_item.filename, file_len))
+                        if field == 'data':
+                            fn = field_item.filename
+                            if fn.startswith('/'):
+                                fn = fn[1:]
+                            f = open(fn, 'w')
+                            f.write(file_data)
+                            f.close()
+                            print 'wrote:'+fn
+                            # config and sched are never saved for testing
+                            if (fn == 'config.json' or fn == 'sched.json'
+                               or fn == 'buttons.json'):
+                                os.remove(fn)
+                            if fn == 'config.json':
+                                config = file_data
+                                try:
+                                    tt = json.loads(file_data)
+                                    host = tt['host']
+                                    x = int(tt['offsetGMT'])
+                                    if x != -1:
+                                        x = offsetGMT
+                                except:
+                                    pass
+                                print('config\n'+file_data)
+                            if fn == 'sched.json':
+                                sched = file_data
+                                print('sched\n'+file_data)
+                            if fn == 'buttons.json':
+                                buttons = file_data
+                                print('buttons\n'+file_data)
+                        del file_data
+                    else:
+                        # Regular form value
+                        print('%s=%s' % (field, form[field].value))
+
+                self.xsend("<html><body>ok</body></html>")
+
+            if self.path == '/update':
+
+                form = cgi.FieldStorage(
+                    fp=self.rfile,
+                    headers=self.headers,
+                    environ={'REQUEST_METHOD': 'POST',
+                             'CONTENT_TYPE': self.headers['Content-Type'],
+                             })
+
+                for field in form.keys():
+                    fields = form[field]
+                    print(fields)
+                    for field_item in fields:
+                        if field_item.filename:
+                            # The field contains an uploaded file
+                            file_data = field_item.file.read()
+                            file_len = len(file_data)
+                            print('Uploaded update %s as "%s" (%d bytes)' %
+                                  (field, field_item.filename, file_len))
+                            # not saving this the update file
+                        else:
+                            # Regular form value
+                            print('%s=%s' % (field_item.name,
+                                  field_item.value))
+
+                self.send_response(301)
+                self.send_header('Location', '/updatesuccessful.html')
+                self.end_headers()
+
+            else:
+                return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+
+os.chdir("data-uncompressed")
+PORT = 8080
+
+Handler = ESPrinkler2RequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+print "serving folder", os.getcwd()
+httpd.serve_forever()
